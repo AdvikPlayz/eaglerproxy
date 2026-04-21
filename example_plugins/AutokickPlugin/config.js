f271a08cf460b27aca3a79b5cfeec5b5e5f2ae64
Advik Kumar <advikk2k15@gmail.com>
2026-04-21 01:46:05 +0000

    Fixing

--- example_plugins/AutokickPlugin/config.js
+++ example_plugins/AutokickPlugin/config.js
@@ -1,7 +1,7 @@
 export const config = {
   // BIND: Where the PROXY listens (Leave this alone or use 8080)
   bindInternalServerPort: 8080,
-  bindInternalServerIp: "0.0.0.0",
+  bindInternalServerIp: "127.0.0.1",
 
   // REMOTE: Where your ATERNOS server is
   remoteServerIp: "DA-BOYZ.aternos.me", // Your Aternos IP here
